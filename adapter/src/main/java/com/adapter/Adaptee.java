60186f76af244cb1e1acaed25c215e65e0b41690
mits0s2019 <mits0s200efta@gmail.com>
2020-03-16 22:51:08 +0200

    adapter pattern

--- adapter/src/main/java/com/adapter/Adaptee.java
+++ adapter/src/main/java/com/adapter/Adaptee.java
@@ -1,11 +1,13 @@
 package com.adapter;
 
-
+import java.util.logging.Logger;
 
 public class Adaptee {
+    private static final Logger logger = Logger.getLogger(Adaptee.class.getName());
 
     public void display1(){
-        System.out.println("Hello from Adaptee class");
+
+        logger.info("Hello from Adaptee class");
     }
 
 }
